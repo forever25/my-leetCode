5172ca6b362f5c6c3570652cc2fe376534dfca18
forever25 <forever__yearn@163.com>
2020-06-16 22:18:01 +0800

    first day

---
+++ 1.两数之和.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode-cn.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (48.71%)
+ * Likes:    8439
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ *
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ *
+ */
+
+// @lc code=start
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let i = nums.length;
+  while (i -- >= 0) {
+    let v = target - nums[i],
+    firstI = nums.indexOf(v)
+    if(i != firstI && firstI > -1) {
+      return [firstI,i]
+    }
+  }
+};
+
+// @lc code=end
